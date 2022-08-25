699633ec053bce864a9368178e15d17c32ae2d29
Marina Klyueva <m.klyueva@headsandhands.ru>
2022-08-26 01:59:06 +0400

    Fix

--- DEDSEC.WPF/obj/Debug/net6.0-windows/Components/LayoutComponent.g.i.cs
+++ DEDSEC.WPF/obj/Debug/net6.0-windows/Components/LayoutComponent.g.i.cs
@@ -53,7 +53,7 @@ namespace DEDSEC.WPF.Components {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DEDSEC.WPF;component/components/layoutcomponent.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DEDSEC.WPF;V1.0.0.0;component/components/layoutcomponent.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Components\LayoutComponent.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
