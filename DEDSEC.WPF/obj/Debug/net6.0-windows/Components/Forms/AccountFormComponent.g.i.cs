38cd27b20910b4429fde2dd600738613aa347e94
Marina Klyueva <m.klyueva@headsandhands.ru>
2022-09-12 22:45:57 +0400

    fix donats views

--- DEDSEC.WPF/obj/Debug/net6.0-windows/Components/Forms/AccountFormComponent.g.i.cs
+++ DEDSEC.WPF/obj/Debug/net6.0-windows/Components/Forms/AccountFormComponent.g.i.cs
@@ -52,7 +52,7 @@ namespace DEDSEC.WPF.Components {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DEDSEC.WPF;component/components/forms/accountformcomponent.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DEDSEC.WPF;V1.0.0.0;component/components/forms/accountformcomponent.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Components\Forms\AccountFormComponent.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
